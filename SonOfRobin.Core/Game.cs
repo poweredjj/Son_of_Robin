39ddaa42251b0eb96c7da9cfe6e6f680274f10e2
Smidowicz Marcin <msmidowi@gemalto.com>
2022-09-12 07:08:13 +0200

    Update Game.cs

--- SonOfRobin.Core/Game.cs
+++ SonOfRobin.Core/Game.cs
@@ -16,7 +16,7 @@ namespace SonOfRobin
     public class SonOfRobinGame : Game
     {
         public static readonly float version = 9.4f;
-        public static readonly DateTime lastChanged = new DateTime(2022, 09, 08);
+        public static readonly DateTime lastChanged = new DateTime(2022, 09, 12);
 
         public static ContentManager content;
 
