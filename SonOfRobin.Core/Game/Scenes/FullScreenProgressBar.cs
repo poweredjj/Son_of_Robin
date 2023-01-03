12b36a418e998ffa0de14881995ef774d9b44b21
Smidowicz Marcin <msmidowi@gemalto.com>
2023-01-03 09:30:07 +0100

    Update FullScreenProgressBar.cs

--- SonOfRobin.Core/Game/Scenes/FullScreenProgressBar.cs
+++ SonOfRobin.Core/Game/Scenes/FullScreenProgressBar.cs
@@ -9,11 +9,11 @@ namespace SonOfRobin
         private readonly Texture2D backgroundTexture;
         private static ContentManager privateContentManager;
 
-        public FullScreenProgressBar() : base(inputType: InputTypes.None, priority: 1, touchLayout: TouchLayout.Empty, tipsLayout: ControlTips.TipsLayout.Empty)
+        public FullScreenProgressBar(string textureName = null) : base(inputType: InputTypes.None, priority: 1, touchLayout: TouchLayout.Empty, tipsLayout: ControlTips.TipsLayout.Empty, blocksDrawsBelow: true)
         {
-            string loadingTextureName = AnimData.loadingGfxNames[SonOfRobinGame.random.Next(0, AnimData.loadingGfxNames.Count)];
+            if (textureName == null) textureName = AnimData.loadingGfxNames[SonOfRobinGame.random.Next(0, AnimData.loadingGfxNames.Count)];
 
-            this.backgroundTexture = privateContentManager.Load<Texture2D>($"gfx/Loading/{loadingTextureName}");
+            this.backgroundTexture = privateContentManager.Load<Texture2D>($"gfx/Loading/{textureName}");
         }
 
         public static void AssignContentManager(ContentManager contentManager)
@@ -35,10 +35,38 @@ namespace SonOfRobin
         public override void Draw()
         {
             SonOfRobinGame.GfxDev.Clear(Color.White);
-            Rectangle testRect = new Rectangle(x: 0, y: 0, width: SonOfRobinGame.VirtualWidth, height: SonOfRobinGame.VirtualHeight);
-            Rectangle destRect = Helpers.DrawTextureInsideRect(texture: this.backgroundTexture, rectangle: testRect, color: Color.White);
 
-            SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientBottom, destinationRectangle: destRect, color: Color.White);
+            int progressRectHeight = (int)(SonOfRobinGame.VirtualHeight * 0.2f);
+
+            Rectangle imageRect = new Rectangle(x: 0, y: 0, width: SonOfRobinGame.VirtualWidth, height: SonOfRobinGame.VirtualHeight - progressRectHeight);
+            Rectangle progressRect = new Rectangle(x: 0, y: imageRect.Height, width: SonOfRobinGame.VirtualWidth, height: progressRectHeight);
+
+            Rectangle offScreenImageRect = imageRect; // imageRect wider than the screen
+            offScreenImageRect.Inflate(offScreenImageRect.Width, 0);
+
+            Rectangle realImageRect = Helpers.DrawTextureInsideRect(texture: this.backgroundTexture, rectangle: offScreenImageRect, color: Color.White * 0.6f);
+            Rectangle bottomGradRect = realImageRect;
+
+            int bottomGradRectHeight = bottomGradRect.Height / 2;
+            int bottomGradRectOffset = bottomGradRect.Height - bottomGradRectHeight;
+            bottomGradRect.Offset(0, bottomGradRectOffset);
+            bottomGradRect.Height = bottomGradRectHeight;
+
+            SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientBottom, destinationRectangle: bottomGradRect, color: Color.White);
+
+            if (realImageRect.Width < imageRect.Width)
+            {
+                // adding gradients on the sides, if background is smaller than the screen
+                int gradWidth = realImageRect.Width / 4;
+
+                Rectangle leftGradRect = new Rectangle(x: realImageRect.X, y: realImageRect.Y, width: gradWidth, height: realImageRect.Height);
+                Rectangle rightGradRect = new Rectangle(x: realImageRect.X + (realImageRect.Width - gradWidth), y: realImageRect.Y, width: gradWidth, height: realImageRect.Height);
+
+                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientLeft, destinationRectangle: leftGradRect, color: Color.White);
+                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientRight, destinationRectangle: rightGradRect, color: Color.White);
+            }
+
+            Helpers.DrawRectangleOutline(rect: progressRect, color: Color.Green, borderWidth: 1);
         }
     }
 }
