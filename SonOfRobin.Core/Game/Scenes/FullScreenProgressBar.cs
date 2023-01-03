7e7e08a89b8ba8a02f2dededda7f9ad576c97d3e
Smidowicz Marcin <msmidowi@gemalto.com>
2023-01-03 12:26:23 +0100

    Update FullScreenProgressBar.cs

--- SonOfRobin.Core/Game/Scenes/FullScreenProgressBar.cs
+++ SonOfRobin.Core/Game/Scenes/FullScreenProgressBar.cs
@@ -53,18 +53,19 @@ namespace SonOfRobin
         }
 
         public override void Update(GameTime gameTime)
-        {
-        }
+        { }
 
         public override void Draw()
         {
-            SonOfRobinGame.GfxDev.Clear(Color.White);
+            Color bgColor = new Color(252, 252, 246);
 
-            int progressRectHeight = (int)(SonOfRobinGame.VirtualHeight * 0.2f);
+            SonOfRobinGame.GfxDev.Clear(bgColor);
+
+            int progressRectHeight = (int)(SonOfRobinGame.VirtualHeight * 0.17f);
 
             Rectangle imageRect = new Rectangle(x: 0, y: 0, width: SonOfRobinGame.VirtualWidth, height: SonOfRobinGame.VirtualHeight - progressRectHeight);
             Rectangle progressRect = new Rectangle(x: 0, y: imageRect.Height, width: SonOfRobinGame.VirtualWidth, height: progressRectHeight);
-            progressRect.Offset(0, -progressRectHeight * 0.25f); // should be slightly above the "correct" position
+            progressRect.Offset(0, -progressRectHeight * 0.35f); // should be slightly above the "correct" position
 
             Rectangle offScreenImageRect = imageRect; // imageRect wider than the screen
             offScreenImageRect.Inflate(offScreenImageRect.Width, 0);
@@ -72,12 +73,12 @@ namespace SonOfRobin
             Rectangle realImageRect = Helpers.DrawTextureInsideRect(texture: this.backgroundTexture, rectangle: offScreenImageRect, color: Color.White * 0.6f);
             Rectangle bottomGradRect = realImageRect;
 
-            int bottomGradRectHeight = (int)(bottomGradRect.Height * 0.8);
+            int bottomGradRectHeight = (int)(bottomGradRect.Height * 0.95);
             int bottomGradRectOffset = bottomGradRect.Height - bottomGradRectHeight;
             bottomGradRect.Offset(0, bottomGradRectOffset);
             bottomGradRect.Height = bottomGradRectHeight;
 
-            SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientBottom, destinationRectangle: bottomGradRect, color: Color.White);
+            SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientBottom, destinationRectangle: bottomGradRect, color: bgColor);
 
             if (realImageRect.Width < imageRect.Width)
             {
@@ -87,15 +88,15 @@ namespace SonOfRobin
                 Rectangle leftGradRect = new Rectangle(x: realImageRect.X, y: realImageRect.Y, width: gradWidth, height: realImageRect.Height);
                 Rectangle rightGradRect = new Rectangle(x: realImageRect.X + (realImageRect.Width - gradWidth), y: realImageRect.Y, width: gradWidth, height: realImageRect.Height);
 
-                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientLeft, destinationRectangle: leftGradRect, color: Color.White);
-                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientRight, destinationRectangle: rightGradRect, color: Color.White);
+                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientLeft, destinationRectangle: leftGradRect, color: bgColor);
+                SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.GradientRight, destinationRectangle: rightGradRect, color: bgColor);
             }
 
-            // SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.WhiteRectangle, destinationRectangle: progressRect, color: Color.Green * 0.25f); // for testing
+            // SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.WhiteRectangle, destinationRectangle: progressRect, color: Color.Brown * 0.25f); // for testing
 
             Rectangle textRect = new Rectangle(
-                x: progressRect.X, y: progressRect.Y + (int)(progressRect.Height * 0.23f),
-                width: progressRect.Width, height: (int)(progressRect.Height * 0.3f));
+                x: progressRect.X, y: progressRect.Y + (int)(progressRect.Height * 0.32f),
+                width: progressRect.Width, height: (int)(progressRect.Height * 0.22f));
 
             textRect.Inflate(-textRect.Width * 0.05f, 0);
 
@@ -103,13 +104,13 @@ namespace SonOfRobin
                 x: progressRect.X, y: progressRect.Y + (progressRect.Height / 2),
                 width: progressRect.Width, height: progressRect.Height / 2);
 
-            progressBarRect.Inflate(-progressBarRect.Width * 0.05f, -progressBarRect.Height * 0.42f);
+            progressBarRect.Inflate(-progressBarRect.Width * 0.05f, -progressBarRect.Height * 0.43f);
             progressBarRect.Width = (int)(progressBarRect.Width * this.ProgressBarPercentage);
 
             // Helpers.DrawRectangleOutline(rect: textRect, color: Color.Blue, borderWidth: 1); // for testing
             // Helpers.DrawRectangleOutline(rect: progressBarRect, color: Color.Cyan, borderWidth: 1); // for testing
 
-            Helpers.DrawTextInsideRect(font: SonOfRobinGame.FontTommy40, text: this.progressBarText, rectangle: textRect, color: barAndTextColor, alignX: Helpers.AlignX.Left, alignY: Helpers.AlignY.Bottom);
+            Helpers.DrawTextInsideRect(font: SonOfRobinGame.FontFreeSansBold24, text: this.progressBarText, rectangle: textRect, color: barAndTextColor, alignX: Helpers.AlignX.Left, alignY: Helpers.AlignY.Bottom);
 
             SonOfRobinGame.SpriteBatch.Draw(texture: SonOfRobinGame.WhiteRectangle, destinationRectangle: progressBarRect, color: barAndTextColor);
         }
