4c89db0d7648ec9f6b07206ae09c1daea70097a5
Smidowicz Marcin <msmidowi@gemalto.com>
2023-02-08 10:08:36 +0100

    Update publish Linux.bat

--- publish Linux.bat
+++ publish Linux.bat
@@ -7,6 +7,9 @@ attrib +R +X ".\Publish\SonOfRobin.DesktopGL"
 
 cd ..
 
-7z.exe a -tzip -r SonOfRobin_Linux.zip .\SonOfRobin.DesktopGL\Publish\SonOfRobin.DesktopGL -mx=5
+7z.exe a -ttar SonOfRobin_Linux.tar .\SonOfRobin.DesktopGL\Publish\SonOfRobin.DesktopGL -mx=5
+7z.exe a SonOfRobin_Linux.tar.gz SonOfRobin_Linux.tar -mx=5
+
+del SonOfRobin_Linux.tar
 
 rmdir /S /Q .\SonOfRobin.DesktopGL\Publish
