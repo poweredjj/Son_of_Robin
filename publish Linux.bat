6164f3059ac0bab9b199aedceff7f8d2da55644d
Marcin Śmidowicz <powered_jj@o2.pl>
2023-03-26 21:27:25 +0200

    linux - update to monogame 3.8.1

--- publish Linux.bat
+++ publish Linux.bat
@@ -1,15 +1,15 @@
 C:
 cd SonOfRobin.DesktopGL
 
-dotnet publish --runtime linux-x64 -c Release -o Publish -p:PublishSingleFile=true /p:PublishReadyToRun=false --self-contained
+dotnet publish --runtime linux-x64 -c Release -p:PublishSingleFile=true -o Publish /p:PublishReadyToRun=false --self-contained
+::-p:PublishSingleFile=true must be on, it won't run on linux otherwise
 
 attrib +R +X ".\Publish\SonOfRobin.DesktopGL"
 
 cd ..
 
-7z.exe a -ttar SonOfRobin_Linux.tar .\SonOfRobin.DesktopGL\Publish\SonOfRobin.DesktopGL -mx=5
+7z.exe a -ttar SonOfRobin_Linux.tar .\SonOfRobin.DesktopGL\Publish\ -mx=5
 7z.exe a SonOfRobin_Linux.tar.gz SonOfRobin_Linux.tar -mx=5
 
 del SonOfRobin_Linux.tar
-
 rmdir /S /Q .\SonOfRobin.DesktopGL\Publish
