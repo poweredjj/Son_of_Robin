cee62c0984748e28e133ac6cdbb2e01d224808ad
Smidowicz Marcin <msmidowi@gemalto.com>
2023-04-27 09:06:49 +0200

    "about" updated

--- SonOfRobin.Core/Game/Scenes/Menu/_MenuTemplate.cs
+++ SonOfRobin.Core/Game/Scenes/Menu/_MenuTemplate.cs
@@ -70,7 +70,7 @@ namespace SonOfRobin
                             }, storeForLaterUse: true));
 
                         aboutTaskChain.Add(new Scheduler.Task(taskName: Scheduler.TaskName.ShowTextWindow, turnOffInputUntilExecution: true, delay: 0, executeHelper: new Dictionary<string, Object> {
-                            { "text", $"| Testers |\n\nFaye\nGlonfindel\nKolowskyyy\nRetro Marek\nShaobell" },
+                            { "text", $"| Testers |\n\nFaye\nGlonfindel\nHellwoman\nKolowskyyy\nRetro Marek\nShaobell" },
                             { "imageList", new List<Texture2D> { PieceInfo.GetTexture(PieceTemplate.Name.DebrisStar), PieceInfo.GetTexture(PieceTemplate.Name.DebrisStar) } },
                             { "bgColor", new List<Byte> { 85, 189, 113 } }
                             }, storeForLaterUse: true));
