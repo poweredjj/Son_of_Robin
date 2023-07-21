2a2194702e027841d2fd740ae2bcbc75d73d9336
Smidowicz Marcin <msmidowi@gemalto.com>
2023-07-21 07:12:29 +0200

    fence recipes updated

--- SonOfRobin.Core/Game.cs
+++ SonOfRobin.Core/Game.cs
@@ -19,7 +19,7 @@ namespace SonOfRobin
     public class SonOfRobinGame : Game
     {
         public const float version = 0.5f;
-        public static readonly DateTime lastChanged = new(2023, 07, 20);
+        public static readonly DateTime lastChanged = new(2023, 07, 21);
 
         public static readonly int enteringIslandGlobalSteps = 4 + Grid.allStagesCount;
         public static ContentManager ContentMgr { get; private set; } // for things other than textures (for textures use TextureBank)
