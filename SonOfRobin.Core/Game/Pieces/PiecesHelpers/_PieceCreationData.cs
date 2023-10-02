a718490d47266508cd5e94ce3eeabddb934fee62
Smidowicz Marcin <msmidowi@gemalto.com>
2023-10-02 14:38:30 +0200

    decreased ruins count (pieces)

--- SonOfRobin.Core/Game/Pieces/PiecesHelpers/_PieceCreationData.cs
+++ SonOfRobin.Core/Game/Pieces/PiecesHelpers/_PieceCreationData.cs
@@ -63,12 +63,12 @@ namespace SonOfRobin
                 new PieceCreationData(name: PieceTemplate.Name.DesertDigSite, multiplier: 0.1f),
                 new PieceCreationData(name: PieceTemplate.Name.GlassDigSite, multiplier: 0.08f, maxAmount: 200),
                 new PieceCreationData(name: PieceTemplate.Name.SwampDigSite, multiplier: 0.11f),
-                new PieceCreationData(name: PieceTemplate.Name.RuinsDigSite, multiplier: 0.14f),
+                new PieceCreationData(name: PieceTemplate.Name.RuinsDigSite, multiplier: 0.12f),
 
                 new PieceCreationData(name: PieceTemplate.Name.Totem, multiplier: 0.1f, maxAmount: 2, doNotReplenish: true),
-                new PieceCreationData(name: PieceTemplate.Name.RuinsColumn, multiplier: 0.08f, doNotReplenish: true),
-                new PieceCreationData(name: PieceTemplate.Name.RuinsRubble, multiplier: 0.15f, doNotReplenish: true),
-                new PieceCreationData(name: PieceTemplate.Name.RuinsWall, multiplier: 0.15f, doNotReplenish: true),
+                new PieceCreationData(name: PieceTemplate.Name.RuinsColumn, multiplier: 0.04f, doNotReplenish: true),
+                new PieceCreationData(name: PieceTemplate.Name.RuinsRubble, multiplier: 0.07f, doNotReplenish: true),
+                new PieceCreationData(name: PieceTemplate.Name.RuinsWall, multiplier: 0.07f, doNotReplenish: true),
 
                 new PieceCreationData(name: PieceTemplate.Name.Rabbit, multiplier: 0.6f, maxAmount: maxAnimalsPerName),
                 new PieceCreationData(name: PieceTemplate.Name.Fox, multiplier: 0.4f, maxAmount: maxAnimalsPerName),
