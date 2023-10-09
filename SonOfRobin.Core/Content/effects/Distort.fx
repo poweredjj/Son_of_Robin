138d064f28791dffbf2067f194dfc3e8e5815b3c
Marcin Śmidowicz <powered_jj@o2.pl>
2023-10-09 19:21:22 +0200

    water caustics updated

--- SonOfRobin.Core/Content/effects/Distort.fx
+++ SonOfRobin.Core/Content/effects/Distort.fx
@@ -11,6 +11,8 @@ Texture2D BaseTexture : register(t0);
 Texture2D DistortTexture : register(t1);
 float4 drawColor;
 float2 baseTextureOffset;
+float2 baseTextureSize;
+float2 baseTextureCorrection;
 sampler s0: register(s0);
 sampler s1: register(s1);
 float distortionMultiplier;
@@ -36,10 +38,14 @@ float4 MainPS(VertexShaderOutput input) : COLOR
 {    
 	// shaders use color value range 0.0f - 1.0f
     
-    float4 distortColor = tex2D(DistortTextureSampler, input.TextureCoordinates + baseTextureOffset);
+    float2 pixelSize = 1.0 / baseTextureSize;
+      
+    float4 distortColor = tex2D(DistortTextureSampler, input.TextureCoordinates + (baseTextureOffset * pixelSize));
     float2 distortionOffset = float2(distortColor.r, distortColor.g) * distortionMultiplier * 0.2f;
+    
+    float2 baseOffset = frac(baseTextureOffset) * pixelSize;
        
-    return tex2D(BaseTextureSampler, input.TextureCoordinates + distortionOffset) * drawColor;   
+    return tex2D(BaseTextureSampler, input.TextureCoordinates + distortionOffset - baseTextureCorrection) * drawColor;
 }
 
 technique SpriteDrawing
--- SonOfRobin.Core/Game/Effects/DistortInstance.cs
+++ SonOfRobin.Core/Game/Effects/DistortInstance.cs
@@ -9,6 +9,7 @@ namespace SonOfRobin
         private readonly Texture2D baseTexture;
         private readonly Texture2D distortTexture;
         private readonly float distortionMultiplier;
+        private readonly Vector2 baseTextureSize;
 
         public DistortInstance(ScrollingSurface scrollingSurface, Texture2D distortTexture, float distortionMultiplier, int framesLeft = 1, int priority = 1) : base(effect: SonOfRobinGame.EffectDistort, framesLeft: framesLeft, priority: priority)
         {
@@ -16,6 +17,7 @@ namespace SonOfRobin
             this.scrollingSurface = scrollingSurface;
             this.baseTexture = this.scrollingSurface.texture;
             this.distortTexture = distortTexture;
+            this.baseTextureSize = new Vector2(this.baseTexture.Width, this.baseTexture.Height);
         }
 
         public override void TurnOn(int currentUpdate, Color drawColor = default)
@@ -24,7 +26,15 @@ namespace SonOfRobin
 
             this.effect.Parameters["distortionMultiplier"].SetValue(this.distortionMultiplier);
             this.effect.Parameters["DistortTexture"].SetValue(this.distortTexture);
-            this.effect.Parameters["baseTextureOffset"].SetValue(this.scrollingSurface.offset / new Vector2(this.baseTexture.Width, this.baseTexture.Height));
+            this.effect.Parameters["baseTextureOffset"].SetValue(this.scrollingSurface.offset);
+
+            Vector2 baseOffsetCorrection = new Vector2(
+                (this.scrollingSurface.offset.X - (int)this.scrollingSurface.offset.X) / this.baseTexture.Width,
+                (this.scrollingSurface.offset.Y - (int)this.scrollingSurface.offset.Y) / this.baseTexture.Height
+                );
+
+            this.effect.Parameters["baseTextureCorrection"].SetValue(baseOffsetCorrection); // fractional part of offset
+            this.effect.Parameters["baseTextureSize"].SetValue(this.baseTextureSize);
 
             // MessageLog.Add(debugMessage: true, text: $"{SonOfRobinGame.CurrentUpdate} offset {this.scrollingSurface.offset}", textColor: Color.Orange);
 
