28817f9e453e3f86770fa9e6ef6c619bbfb5024e
Smidowicz Marcin <msmidowi@gemalto.com>
2023-10-18 08:36:31 +0200

    separate levels support added (incomplete)

--- SonOfRobin.Core/Game/Pieces/PiecesHelpers/_PieceInfo.cs
+++ SonOfRobin.Core/Game/Pieces/PiecesHelpers/_PieceInfo.cs
@@ -2944,7 +2944,7 @@ namespace SonOfRobin
                         this.category = BoardPiece.Category.Indestructible;
                         this.boardTask = Scheduler.TaskName.UseEntrance;
                         this.interactVirtButtonName = TextureBank.TextureName.VirtButtonEnterExit;
-                        this.allowedDensity = new AllowedDensity(radius: 100, maxNoOfPiecesBlocking: 0);
+                        this.allowedDensity = new AllowedDensity(radius: 1000, maxNoOfPiecesSameClass: 0);
                         this.blocksMovement = true;
                         this.hasFlatShadow = true;
                         break;
