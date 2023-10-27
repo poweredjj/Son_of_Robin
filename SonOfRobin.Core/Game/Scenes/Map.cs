086310cd498709a237e6da1e86f1b5e56b8cedae
Smidowicz Marcin <msmidowi@gemalto.com>
2023-10-27 11:39:54 +0200

    map marker scale fixed

--- SonOfRobin.Core/Game/Scenes/Map.cs
+++ SonOfRobin.Core/Game/Scenes/Map.cs
@@ -729,7 +729,7 @@ namespace SonOfRobin
 
             if (this.Mode == MapMode.Full)
             {
-                int markerSizePixels = (int)(Preferences.MapMarkerRealSize * this.viewParams.ScaleY / Preferences.GlobalScale);
+                int markerSizePixels = (int)(Preferences.MapMarkerRealSize * this.viewParams.ScaleY);
                 Rectangle markerRect = new Rectangle(x: 0, y: 0, width: markerSizePixels, height: markerSizePixels);
 
                 foreach (var kvp in this.world.ActiveLevel.mapMarkerByColor)
