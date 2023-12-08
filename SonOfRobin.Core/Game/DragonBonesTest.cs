97c435ea08ed8e0d70f1126156b96f9e5cd265b4
Smidowicz Marcin <msmidowi@gemalto.com>
2023-12-08 16:54:44 +0100

    DragonBones updated (incomplete)

--- SonOfRobin.Core/Game/DragonBonesTest.cs
+++ SonOfRobin.Core/Game/DragonBonesTest.cs
@@ -16,6 +16,8 @@ namespace SonOfRobin
                 var demonAtlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/DemonTexture.json");
                 demonAtlas.LoadContent(SonOfRobinGame.ContentMgr);
                 demonArmature = DragonBones.FromJson("Content/gfx/_DragonBones/Demon.json", demonAtlas, SonOfRobinGame.GfxDev).Armature;
+                demonArmature.GotoAndPlay("run");
+
             }
 
             if (sheepArmature == null)
@@ -23,14 +25,13 @@ namespace SonOfRobin
                 var sheepAtlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/Sheep_Ani_tex.json");
                 sheepAtlas.LoadContent(SonOfRobinGame.ContentMgr);
                 sheepArmature = DragonBones.FromJson("Content/gfx/_DragonBones/Sheep_Ani_ske.json", sheepAtlas, SonOfRobinGame.GfxDev).Armature;
-
-                sheepArmature.GotoAndPlay("goat_walk_anim");
+                // sheepArmature.GotoAndPlay("goat_walk_anim");
             }
 
-            //demonArmature.Update(SonOfRobinGame.CurrentGameTime.TotalGameTime);
-            //sheepArmature.Update(SonOfRobinGame.CurrentGameTime.TotalGameTime);
+            demonArmature.Update(SonOfRobinGame.CurrentGameTime.TotalGameTime);
+            // sheepArmature.Update(SonOfRobinGame.CurrentGameTime.TotalGameTime);
 
-            sheepArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(200f, 250f), rotation: 0f, scale: new Vector2(0.5f, 0.3f), color: Color.White);
+            //sheepArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(200f, 250f), rotation: 0f, scale: new Vector2(0.5f, 0.3f), color: Color.White);
             demonArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(300f, 220f), rotation: 0f, scale: new Vector2(0.3f, 0.3f), color: Color.White);
         }
     }
