2446990bbf533aeed469b65bc05041606a7c83ed
Smidowicz Marcin <msmidowi@gemalto.com>
2023-12-08 15:55:12 +0100

    switched to DragonBonesMG repo

--- DragonBonesMG/Display/TexturePart.cs
+++ DragonBonesMG/Display/TexturePart.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
-namespace DragonBonesMG.Display {
-    public class TexturePart : IDrawableDb {
+namespace DragonBonesMG.Display
+{
+    public class TexturePart : IDrawableDb
+    {
 
         private readonly Texture2D _texture;
         private readonly Rectangle _bounds;
 
-        public TexturePart(Texture2D texture, Rectangle bounds) {
+        public TexturePart(Texture2D texture, Rectangle bounds)
+        {
             if (texture == null) throw new ArgumentNullException(nameof(texture));
             _texture = texture;
             _bounds = bounds;
@@ -17,17 +20,19 @@ namespace DragonBonesMG.Display {
         /// <summary>
         /// Assumes SpriteBatch.Begin() has been called.
         /// </summary>
-        public void Draw(SpriteBatch s) {
+        public void Draw(SpriteBatch s)
+        {
             // Use center of the texture as the origin
             s.Draw(_texture, -_bounds.Size.ToVector2() / 2f, _bounds, Color.White);
         }
 
-        public void Draw(SpriteBatch s, Color color, SpriteEffects effect = SpriteEffects.None) {
-            s.Draw(_texture, -_bounds.Size.ToVector2() / 2f, sourceRectangle: _bounds,
-                color: color, effects: effect);
+        public void Draw(SpriteBatch s, Color color, SpriteEffects effect = SpriteEffects.None)
+        {
+            s.Draw(texture: _texture, position: -_bounds.Size.ToVector2() / 2f, scale: Vector2.One, sourceRectangle: _bounds, color: color, rotation: 0, origin: Vector2.Zero, effects: effect, layerDepth: 0);
         }
 
-        public Texture2D RenderToTexture(SpriteBatch s) {
+        public Texture2D RenderToTexture(SpriteBatch s)
+        {
             var graphics = s.GraphicsDevice;
             var prevTargets = graphics.GetRenderTargets();
 
@@ -35,7 +40,7 @@ namespace DragonBonesMG.Display {
             graphics.SetRenderTarget(target);
             s.Begin();
 
-            s.Draw(_texture, null, target.Bounds);
+            s.Draw(texture: _texture, destinationRectangle: target.Bounds, color: Color.White);
 
             s.End();
             graphics.SetRenderTargets(prevTargets);
