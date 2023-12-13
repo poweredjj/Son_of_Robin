722dd73cb59c9e0defb2a84a875fd2e3a89d5c00
Smidowicz Marcin <msmidowi@gemalto.com>
2023-12-13 10:11:17 +0100

    DragonBones updated (incomplete)

--- DragonBonesMG/Animation/DbAnimation.cs
+++ DragonBonesMG/Animation/DbAnimation.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using DragonBonesMG.Core;
+﻿using DragonBonesMG.Core;
 using DragonBonesMG.JsonData;
-using DragonBonesMG.Util;
+using System;
+using System.Linq;
 
-namespace DragonBonesMG.Animation {
+namespace DragonBonesMG.Animation
+{
     /// <summary>
     /// DragonBones animations consist of 4 timelines (for now). A <see cref="TransformTimeline"/>,
     /// a <see cref="DisplayTimeline"/>, an <see cref="FFDTimeline"/> and an EventTimeline (as an array of <see cref="EventFrames"/>).
-    /// An armatures <see cref="IAnimatable"/> methods will delegate to a DbAnimation (if one is active). 
-    /// <see cref="Update"/> will update all timelines. 
+    /// An armatures <see cref="IAnimatable"/> methods will delegate to a DbAnimation (if one is active).
+    /// <see cref="Update"/> will update all timelines.
     /// <see cref="GetCurrentState"/> returns a snapshot of current timeline states (except for events, those are triggered
     /// automatically when a keyframe is passed).
     /// </summary>
-    public class DbAnimation {
+    public class DbAnimation
+    {
         public string Name { get; }
         public int Duration { get; }
-        public float DurationTime => Duration / (float) FrameRate;
+        public float DurationTime => Duration / (float)FrameRate;
+
         // Not sure if this is ever useful in a game; so not using this. Instead I use Loop below.
         public readonly int PlayTimes;
+
         public bool Loop { get; internal set; }
         internal int FrameRate => _armature.FrameRate;
         public float CurrentFrame { get; private set; }
@@ -38,7 +40,8 @@ namespace DragonBonesMG.Animation {
 
         #region Constructor
 
-        internal DbAnimation(DbArmature armature, AnimationData data) {
+        internal DbAnimation(DbArmature armature, AnimationData data)
+        {
             _armature = armature;
             Name = data.Name;
             Duration = data.Duration;
@@ -51,21 +54,23 @@ namespace DragonBonesMG.Animation {
             _ffdTimeline = new FFDTimeline(data.MeshTimelines);
             EventFrames = new EventFrame[data.EventFrames.Length];
             var startFrame = 0;
-            for (var i = 0; i < data.EventFrames.Length; i++) {
+            for (var i = 0; i < data.EventFrames.Length; i++)
+            {
                 var frame = data.EventFrames[i];
                 EventFrames[i] = new EventFrame(startFrame, frame);
                 startFrame += frame.Duration;
             }
         }
 
-        #endregion
+        #endregion Constructor
 
         #region Playback
 
         /// <summary>
         /// Play this animation.
         /// </summary>
-        public void Play(bool loop) {
+        public void Play(bool loop)
+        {
             IsPlaying = true;
             IsComplete = false;
             Loop = loop;
@@ -74,38 +79,52 @@ namespace DragonBonesMG.Animation {
         /// <summary>
         /// Pause this animation.
         /// </summary>
-        public void Pause() {
+        public void Pause()
+        {
             IsPlaying = false;
         }
 
-        public void Reset() {
+        public void Reset()
+        {
             IsPlaying = false;
             CurrentFrame = 0;
             IsComplete = false;
         }
 
-        internal void PassTime(float elapsed) {
+        internal void PassTime(float elapsed)
+        {
             // check for events
-            var lastFrame = CurrentFrame;
+            float lastFrame = CurrentFrame;
             CurrentFrame += elapsed * FrameRate;
-            if (elapsed > 0) {
+            if (elapsed > 0)
+            {
                 // positive timescale
-                if (CurrentFrame >= Duration) {
-                    if (Loop) {
+                if (CurrentFrame >= Duration)
+                {
+                    if (Loop)
+                    {
                         CurrentFrame -= Duration;
-                    } else {
+                    }
+                    else
+                    {
                         IsPlaying = false;
                         IsComplete = true;
                         CurrentFrame = Duration;
                     }
                 }
                 TriggerEvents(lastFrame, CurrentFrame);
-            } else {
+            }
+            else
+            {
                 // for negative timescales
-                if (CurrentFrame < 0) {
-                    if (Loop) {
+                if (CurrentFrame < 0)
+                {
+                    if (Loop)
+                    {
                         CurrentFrame += Duration;
-                    } else {
+                    }
+                    else
+                    {
                         IsPlaying = false;
                         IsComplete = true;
                         CurrentFrame = 0;
@@ -119,38 +138,41 @@ namespace DragonBonesMG.Animation {
         /// Set the animation time. Used instead of <see cref="PassTime"/> when no events should be triggered.
         /// </summary>
         /// <param name="time"></param>
-        internal void SetTime(float time) {
+        internal void SetTime(float time)
+        {
             if (time < 0) throw new ArgumentException(nameof(time) + " can not be negative");
             CurrentFrame = (time * FrameRate) % Duration;
         }
 
-        #endregion
+        #endregion Playback
 
         #region Update
 
-        public void Update(TimeSpan elapsed) {
+        public void Update(TimeSpan elapsed)
+        {
             if (!IsPlaying || elapsed == TimeSpan.Zero) return;
-            PassTime((float) (TimeScale * elapsed.TotalSeconds));
+            PassTime((float)(TimeScale * elapsed.TotalSeconds));
             _transformTimeline.Update(CurrentFrame);
             _displayTimeline.Update(CurrentFrame);
             _ffdTimeline.Update(CurrentFrame);
         }
 
-        #endregion
+        #endregion Update
 
         #region GetCurrentState
 
         /// <summary>
         /// Calculate a snapshot of the animation at this instant and return it.
         /// </summary>
-        internal DbAnimationState GetCurrentState() {
+        internal DbAnimationState GetCurrentState()
+        {
             return new DbAnimationState(
                 _transformTimeline.GetState(),
                 _displayTimeline.GetState(),
                 _ffdTimeline.GetState());
         }
 
-        #endregion
+        #endregion GetCurrentState
 
         #region Events
 
@@ -161,39 +183,38 @@ namespace DragonBonesMG.Animation {
         /// </summary>
         /// <param name="start"></param>
         /// <param name="end"></param>
-        private void TriggerEvents(float start, float end) {
+        private void TriggerEvents(float start, float end)
+        {
             var range = new CyclicFloatRange(start, end);
             foreach (var frame in EventFrames.Where(e => range.Inside(e.StartFrame)))
                 TriggerEvent(frame);
         }
 
-        private void TriggerEvent(EventFrame e) {
-            if (e.Action != null)
-                _armature.OnAction(new DbAnimationEventArgs(e.Action));
-            if (e.Event != null)
-                _armature.OnAnimationEvent(new DbAnimationEventArgs(e.Event));
-            if (e.Sound != null)
-                _armature.OnSound(new DbAnimationEventArgs(e.Sound));
+        private void TriggerEvent(EventFrame e)
+        {
+            if (e.Action != null) _armature.OnAction(new DbAnimationEventArgs(e.Action));
+            if (e.Event != null) _armature.OnAnimationEvent(new DbAnimationEventArgs(e.Event));
+            if (e.Sound != null) _armature.OnSound(new DbAnimationEventArgs(e.Sound));
         }
 
-        private struct CyclicFloatRange {
-
+        private struct CyclicFloatRange
+        {
             public readonly float Start;
             public readonly float End;
 
-            public CyclicFloatRange(float start, float end) {
+            public CyclicFloatRange(float start, float end)
+            {
                 Start = start;
                 End = end;
             }
 
-            public bool Inside(float value) {
-                if (Start <= End)
-                    return Start < value && value <= End;
+            public bool Inside(float value)
+            {
+                if (Start <= End) return Start < value && value <= End;
                 return Start < value || value <= End;
             }
-
         }
 
-        #endregion
+        #endregion Events
     }
 }
--- SonOfRobin.Core/Game/Scenes/DragonBonesTestScene.cs
+++ SonOfRobin.Core/Game/Scenes/DragonBonesTestScene.cs
@@ -1,85 +1,66 @@
 ﻿using DragonBonesMG;
+using DragonBonesMG.Core;
 using DragonBonesMG.Display;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SonOfRobin
 {
     public class DragonBonesTestScene : Scene
     {
-        private readonly DragonBonesMG.Core.DbArmature demonArmature;
-        private readonly string[] demonAnimNames;
+        public readonly struct DragonBonesAnim
+        {
+            private readonly TextureAtlas textureAtlas;
+            private readonly DbArmature dbArmature;
+            private readonly string[] animNames;
 
-        private readonly DragonBonesMG.Core.DbArmature sheepArmature;
-        private readonly string[] sheepAnimNames;
+            public DragonBonesAnim(string atlasPath, string skeletonPath)
+            {
+                this.textureAtlas = TextureAtlas.FromJson(atlasPath);
 
-        private readonly DragonBonesMG.Core.DbArmature dragonArmature;
-        private readonly string[] dragonAnimNames;
+                this.textureAtlas.LoadContent(SonOfRobinGame.ContentMgr);
+                this.dbArmature = DragonBones.FromJson(skeletonPath, this.textureAtlas, SonOfRobinGame.GfxDev).Armature;
+                this.animNames = this.dbArmature.Animations.Select(a => a.Name).ToArray();
+                this.dbArmature.GotoAndPlay(animation: this.animNames[0], loop: false);
+            }
 
-        private readonly DragonBonesMG.Core.DbArmature mechaArmature;
-        private readonly string[] mechaAnimNames;
+            public void Update()
+            {
+                this.dbArmature.Update(SonOfRobinGame.CurrentGameTime.ElapsedGameTime);
+
+                if (this.dbArmature.IsDoneAnimating())
+                {
+                    string animName = this.animNames[SonOfRobinGame.random.Next(this.animNames.Length)];
+                    this.dbArmature.GotoAndPlay(animation: animName, loop: false);
+                }
+            }
+
+            public void Draw(Vector2 position, Vector2 scale)
+            {
+                this.dbArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: position, rotation: 0f, scale: scale, color: Color.White);
+            }
+        }
+
+        private readonly DragonBonesAnim[] dragonBonesAnims;
 
         public DragonBonesTestScene(int priority = 0) : base(inputType: InputTypes.None, priority: priority, blocksUpdatesBelow: false, alwaysUpdates: false, alwaysDraws: false, touchLayout: TouchLayout.Empty, tipsLayout: ControlTips.TipsLayout.Empty)
         {
-            {
-                TextureAtlas atlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/DemonTexture.json");
-                atlas.LoadContent(SonOfRobinGame.ContentMgr);
-                this.demonArmature = DragonBones.FromJson("Content/gfx/_DragonBones/Demon.json", atlas, SonOfRobinGame.GfxDev).Armature;
-                this.demonAnimNames = this.demonArmature.Animations.Select(a => a.Name).ToArray();
-                this.demonArmature.GotoAndPlay(animation: this.demonAnimNames[0], loop: false);
-            }
-            {
-                TextureAtlas atlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/Sheep_tex.json");
-                atlas.LoadContent(SonOfRobinGame.ContentMgr);
-                this.sheepArmature = DragonBones.FromJson("Content/gfx/_DragonBones/Sheep_ske.json", atlas, SonOfRobinGame.GfxDev).Armature;
-                this.sheepAnimNames = this.sheepArmature.Animations.Select(a => a.Name).ToArray();
-                this.sheepArmature.GotoAndPlay(animation: this.sheepAnimNames[0], loop: false);
-            }
-            {
-                TextureAtlas atlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/Dragon_tex.json");
-                atlas.LoadContent(SonOfRobinGame.ContentMgr);
-                this.dragonArmature = DragonBones.FromJson("Content/gfx/_DragonBones/Dragon_ske.json", atlas, SonOfRobinGame.GfxDev).Armature;
-                this.dragonAnimNames = this.dragonArmature.Animations.Select(a => a.Name).ToArray();
-                this.dragonArmature.GotoAndPlay(animation: this.dragonAnimNames[0], loop: false);
-            }
-            {
-                TextureAtlas atlas = TextureAtlas.FromJson("Content/gfx/_DragonBones/mecha_1004d_show_tex.json");
-                atlas.LoadContent(SonOfRobinGame.ContentMgr);
-                this.mechaArmature = DragonBones.FromJson("Content/gfx/_DragonBones/mecha_1004d_show_ske.json", atlas, SonOfRobinGame.GfxDev).Armature;
-                this.mechaAnimNames = this.mechaArmature.Animations.Select(a => a.Name).ToArray();
-                this.mechaArmature.GotoAndPlay(animation: this.mechaAnimNames[0], loop: false);
-            }
+            List<DragonBonesAnim> dragonBonesAnimsList = new();
+            dragonBonesAnimsList.Add(new DragonBonesAnim(atlasPath: "Content/gfx/_DragonBones/DemonTexture.json", skeletonPath: "Content/gfx/_DragonBones/Demon.json"));
+            dragonBonesAnimsList.Add(new DragonBonesAnim(atlasPath: "Content/gfx/_DragonBones/Sheep_tex.json", skeletonPath: "Content/gfx/_DragonBones/Sheep_ske.json"));
+            dragonBonesAnimsList.Add(new DragonBonesAnim(atlasPath: "Content/gfx/_DragonBones/Dragon_tex.json", skeletonPath: "Content/gfx/_DragonBones/Dragon_ske.json"));
+            dragonBonesAnimsList.Add(new DragonBonesAnim(atlasPath: "Content/gfx/_DragonBones/mecha_1004d_show_tex.json", skeletonPath: "Content/gfx/_DragonBones/mecha_1004d_show_ske.json"));
+            //dragonBonesAnimsList.Add(new DragonBonesAnim(atlasPath: "Content/gfx/_DragonBones/Ubbie_tex.json", skeletonPath: "Content/gfx/_DragonBones/Ubbie_ske.json"));
+
+            this.dragonBonesAnims = dragonBonesAnimsList.ToArray();
         }
 
         public override void Update()
         {
-            this.demonArmature.Update(SonOfRobinGame.CurrentGameTime.ElapsedGameTime);
-            this.sheepArmature.Update(SonOfRobinGame.CurrentGameTime.ElapsedGameTime);
-            this.dragonArmature.Update(SonOfRobinGame.CurrentGameTime.ElapsedGameTime);
-            this.mechaArmature.Update(SonOfRobinGame.CurrentGameTime.ElapsedGameTime);
-
-            if (demonArmature.IsDoneAnimating())
+            foreach (DragonBonesAnim dragonBonesAnim in this.dragonBonesAnims)
             {
-                string animName = this.demonAnimNames[SonOfRobinGame.random.Next(demonAnimNames.Length)];
-                demonArmature.GotoAndPlay(animation: animName, loop: false);
-            }
-
-            if (sheepArmature.IsDoneAnimating())
-            {
-                string animName = this.sheepAnimNames[SonOfRobinGame.random.Next(sheepAnimNames.Length)];
-                sheepArmature.GotoAndPlay(animation: animName, loop: false);
-            }
-
-            if (dragonArmature.IsDoneAnimating())
-            {
-                string animName = this.dragonAnimNames[SonOfRobinGame.random.Next(dragonAnimNames.Length)];
-                dragonArmature.GotoAndPlay(animation: animName, loop: false);
-            }
-
-            if (mechaArmature.IsDoneAnimating())
-            {
-                string animName = this.mechaAnimNames[SonOfRobinGame.random.Next(mechaAnimNames.Length)];
-                mechaArmature.GotoAndPlay(animation: animName, loop: false);
+                dragonBonesAnim.Update();
             }
         }
 
@@ -87,10 +68,12 @@ namespace SonOfRobin
         {
             float scale = (float)SonOfRobinGame.VirtualHeight / 250f;
 
-            //this.demonArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(120f, 220f) * scale, rotation: 0f, scale: new Vector2(-0.35f, 0.35f) * scale, color: Color.White);
-            this.sheepArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(300f, 150f) * scale, rotation: 0f, scale: new Vector2(0.2f, 0.2f) * scale, color: Color.White);
-            //this.dragonArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(450f, 150f) * scale, rotation: 0f, scale: new Vector2(0.2f, 0.2f) * scale, color: Color.White);
-            //this.mechaArmature.Draw(s: SonOfRobinGame.SpriteBatch, position: new Vector2(520f, 210f) * scale, rotation: 0f, scale: new Vector2(0.4f, 0.4f) * scale, color: Color.White);
+            int animNo = 0;
+            foreach (DragonBonesAnim dragonBonesAnim in this.dragonBonesAnims)
+            {
+                dragonBonesAnim.Draw(position: new Vector2(125 * (animNo + 1), 150) * scale, scale: new Vector2(0.2f, 0.2f) * scale);
+                animNo++;
+            }
         }
     }
 }
