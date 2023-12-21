ef1306c70a2389a366ceb2a59d37b3054aafa267
Marcin Śmidowicz <powered_jj@o2.pl>
2023-12-21 16:03:45 +0100

    fixed android version crash at start

--- SonOfRobin.Core/Game.cs
+++ SonOfRobin.Core/Game.cs
@@ -39,7 +39,7 @@ namespace SonOfRobin
 
         private static void MoveWindowOnWorkMachine(Game game)
         {
-            if (ThisIsWorkMachine) game.Window.Position = new Point(-7, 758); // COMMENT THIS LINE on ANDROID
+           //  if (ThisIsWorkMachine) game.Window.Position = new Point(-7, 758); // COMMENT THIS LINE on ANDROID
         }
 
         public static GraphicsDeviceManager GfxDevMgr { get; private set; }
