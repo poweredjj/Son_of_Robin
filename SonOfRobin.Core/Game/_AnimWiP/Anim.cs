07594605f7c59752b23f2c9786c4a2007ce69fa6
Smidowicz Marcin <msmidowi@gemalto.com>
2023-12-29 13:02:44 +0100

    create new anims system (incomplete)

---
+++ SonOfRobin.Core/Game/_AnimWiP/Anim.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace SonOfRobin
+{
+    public readonly struct Anim
+    {
+        public readonly string name;
+        public readonly int size;
+        public readonly AnimFrame[] frameArray;
+        public readonly bool isLooped;
+        public readonly int duration;
+        public readonly bool switchWhenComplete;
+        public readonly string switchName;
+
+        public Anim(string name, int size, AnimFrame[] frameArray, bool isLooped, string switchName = "")
+        {
+            this.name = name;
+            this.size = size;
+            this.frameArray = frameArray;
+            this.isLooped = isLooped;
+            this.duration = frameArray.Select(f => (int)f.duration).Sum();
+            this.switchWhenComplete = switchName != "";
+            this.switchName = switchName;
+        }
+    }
+}
---
+++ SonOfRobin.Core/Game/_AnimWiP/AnimPkg.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace SonOfRobin
+{
+    public class AnimPkg
+    {
+        public readonly Rectangle colRect;
+        public readonly AnimFrame croppedFrame;
+        private readonly Dictionary<string, Anim> animByName;
+        public readonly bool leftRightOnly;
+
+        public AnimPkg(Rectangle colRect, AnimFrame croppedFrame, bool leftRightOnly = false)
+        {
+            this.colRect = colRect;
+            this.animByName = new Dictionary<string, Anim>();
+            this.croppedFrame = croppedFrame;
+            this.leftRightOnly = leftRightOnly;
+        }
+
+        public void AddAnim(Anim anim)
+        {
+            animByName[anim.name] = anim;
+        }
+
+        public Anim GetAnim(string name)
+        {
+            return animByName[name];
+        }
+    }
+}
