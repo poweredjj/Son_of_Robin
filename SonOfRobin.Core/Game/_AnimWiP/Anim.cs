09de04961991c00bf2739c212bacab356d2e9bc2
Smidowicz Marcin <msmidowi@gemalto.com>
2023-12-29 13:38:34 +0100

    update new anims system (incomplete)

--- SonOfRobin.Core/Game/_AnimWiP/Anim.cs
+++ SonOfRobin.Core/Game/_AnimWiP/Anim.cs
@@ -6,18 +6,34 @@ namespace SonOfRobin
     {
         public readonly string name;
         public readonly int size;
-        public readonly AnimFrame[] frameArray;
-        public readonly bool isLooped;
+        public readonly bool looped;
+        public readonly bool pingPong;
         public readonly int duration;
         public readonly bool switchWhenComplete;
         public readonly string switchName;
+        public readonly AnimFrame[] frameArray;
 
-        public Anim(string name, int size, AnimFrame[] frameArray, bool isLooped, string switchName = "")
+        public Anim(string name, int size, AnimFrame[] frameArray, bool looped, bool pingPong, string switchName = "")
         {
             this.name = name;
             this.size = size;
+            this.looped = looped;
+            this.pingPong = pingPong;
+
+            if (looped)
+            {
+                AnimFrame[] frameArrayLooped = new AnimFrame[frameArray.Length * 2];
+
+                for (int i = 0; i < frameArray.Length; i++)
+                {
+                    frameArrayLooped[i] = frameArray[i];
+                    frameArrayLooped[frameArray.Length + (frameArray.Length - i - 1)] = frameArray[i];
+                }
+
+                frameArray = frameArrayLooped;
+            }
+
             this.frameArray = frameArray;
-            this.isLooped = isLooped;
             this.duration = frameArray.Select(f => (int)f.duration).Sum();
             this.switchWhenComplete = switchName != "";
             this.switchName = switchName;
--- SonOfRobin.Core/Game/_AnimWiP/AnimPkg.cs
+++ SonOfRobin.Core/Game/_AnimWiP/AnimPkg.cs
@@ -5,27 +5,30 @@ namespace SonOfRobin
 {
     public class AnimPkg
     {
+        public readonly AnimData.PkgName pkgName;
         public readonly Rectangle colRect;
         public readonly AnimFrame croppedFrame;
-        private readonly Dictionary<string, Anim> animByName;
+        private readonly Dictionary<int, Dictionary<string, Anim>> animDict;
         public readonly bool leftRightOnly;
 
-        public AnimPkg(Rectangle colRect, AnimFrame croppedFrame, bool leftRightOnly = false)
+        public AnimPkg(AnimData.PkgName pkgName, Rectangle colRect, AnimFrame croppedFrame, bool leftRightOnly = false)
         {
+            this.pkgName = pkgName;
             this.colRect = colRect;
-            this.animByName = new Dictionary<string, Anim>();
+            this.animDict = new Dictionary<int, Dictionary<string, Anim>>();
             this.croppedFrame = croppedFrame;
             this.leftRightOnly = leftRightOnly;
         }
 
         public void AddAnim(Anim anim)
         {
-            animByName[anim.name] = anim;
+            if (!this.animDict.ContainsKey(anim.size)) this.animDict[anim.size] = new Dictionary<string, Anim>();
+            this.animDict[anim.size][anim.name] = anim;
         }
 
-        public Anim GetAnim(string name)
+        public Anim GetAnim(int size, string name)
         {
-            return animByName[name];
+            return this.animDict[size][name];
         }
     }
 }
