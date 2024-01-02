9707a20d9a86abc71c2be198a1272278ec46d6a1
Smidowicz Marcin <msmidowi@gemalto.com>
2024-01-02 12:39:37 +0100

    update to net8.0

--- publish Windows.bat
+++ publish Windows.bat
@@ -4,12 +4,12 @@ dotnet publish -c Release -r win-x64 /p:PublishReadyToRun=false /p:TieredCompila
 
 cd ..
 
-move .\SonOfRobin.WindowsDX\bin\Release\net6.0-windows\win-x64\publish ".\SonOfRobin.WindowsDX\bin\Release\net6.0-windows\win-x64\Son_of_Robin"
+move .\SonOfRobin.WindowsDX\bin\Release\net8.0-windows7.0\win-x64\publish ".\SonOfRobin.WindowsDX\bin\Release\net8.0-windows7.0\win-x64\Son_of_Robin"
 
-7z.exe a SonOfRobin_Windows.zip ".\SonOfRobin.WindowsDX\bin\Release\net6.0-windows\win-x64\Son_of_Robin" -tzip -mx=9
+7z.exe a SonOfRobin_Windows.zip ".\SonOfRobin.WindowsDX\bin\Release\net8.0-windows7.0\win-x64\Son_of_Robin" -tzip -mx=9
 
 cd "_batches"
 ..\7z a -tzip ..\SonOfRobin_Windows.zip "Son of Robin.bat"
 cd ..
 
-rmdir /S /Q .\SonOfRobin.WindowsDX\bin\Release\net6.0-windows\win-x64
+rmdir /S /Q .\SonOfRobin.WindowsDX\bin\Release\net8.0-windows7.0\win-x64
