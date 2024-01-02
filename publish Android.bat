ec5f5b0e2c27fd2049477a9559768ba92f84d398
Marcin Śmidowicz <powered_jj@o2.pl>
2024-01-02 18:11:16 +0100

    add android publish batch

---
+++ publish Android.bat
@@ -0,0 +1,6 @@
+C:
+cd SonOfRobin.Android
+
+dotnet publish -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=AhoyGames.keystore -p:AndroidSigningKeyAlias=AhoyGames -p:AndroidSigningKeyPass=Korzen321 -p:AndroidSigningStorePass=Korzen321
+
+move bin\Release\net8.0-android\publish\SonOfRobin.SonOfRobin-Signed.apk ..\
