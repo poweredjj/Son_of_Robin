90cb03048c4bb329374699b1da3f655e43ab75fc
Marcin Śmidowicz <powered_jj@o2.pl>
2024-01-22 22:20:04 +0100

    fix marker positions at screen edges

--- SonOfRobin.Core/Game/Scenes/PlayerPanel.cs
+++ SonOfRobin.Core/Game/Scenes/PlayerPanel.cs
@@ -315,11 +315,13 @@ namespace SonOfRobin
 
                         markerPos += offset;
 
+                        markerPos += new Vector2(markerWidth / 2, markerHeight / 2);
+
                         Vector2 markerScreenPos = this.world.TranslateWorldToScreenPos(markerPos);
                         markerScreenPos.X -= this.viewParams.DrawPos.X;
                         markerScreenPos.Y -= this.viewParams.DrawPos.Y;
 
-                        Vector2 markerPosRightBottom = new(markerPos.X, markerPos.Y);
+                        Vector2 markerPosRightBottom = new(markerPos.X + markerWidth, markerPos.Y + markerHeight);
                         Vector2 markerScreenPosRightBottom = this.world.TranslateWorldToScreenPos(markerPosRightBottom);
                         markerScreenPosRightBottom.X -= this.viewParams.DrawPos.X;
                         markerScreenPosRightBottom.Y -= this.viewParams.DrawPos.Y;
