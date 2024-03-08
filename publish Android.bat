3c3ee3a93bbc7ebcfbddf4dce7ce2eb4a7047ddd
Smidowicz Marcin <msmidowi@gemalto.com>
2024-03-08 11:00:32 +0100

    update publish batches

--- publish Android.bat
+++ publish Android.bat
@@ -4,3 +4,5 @@ cd SonOfRobin.Android
 dotnet publish -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=AhoyGames.keystore -p:AndroidSigningKeyAlias=AhoyGames -p:AndroidSigningKeyPass=Korzen321 -p:AndroidSigningStorePass=Korzen321
 
 move bin\Release\net8.0-android\publish\SonOfRobin.SonOfRobin-Signed.apk ..\
+
+cd ..
